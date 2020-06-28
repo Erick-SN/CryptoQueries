165b2936e9e3421009d29c51b55fcb8db26119c8
Erick-SN <ericksn0707@gmail.com>
2020-06-28 08:56:12 -0500

    Custom hook

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import styled from '@emotion/styled';
-
+import useCoin from '../hooks/useCoin';
 const Button = styled.input`
   margin-top: 20px;
   font-weight: bold;
@@ -19,9 +19,17 @@ const Button = styled.input`
 `;
 
 const Form = () => {
+  const COINS = [
+    { code: 'USD', name: 'American Dollar' },
+    { code: 'MXN', name: 'Mexican Peso' },
+    { code: 'EUR', name: 'Euro' },
+    { code: 'GBP', name: 'Pound Sterling' },
+  ];
+  const [coin, CoinSelect, setCoin] = useCoin('Choose your Coin', '', COINS);
   return (
     <>
       <form>
+        <CoinSelect />
         <Button type='submit' value='Compute' />
       </form>
     </>
---
+++ src/hooks/useCoin.jsx
@@ -0,0 +1,21 @@
+import React, { useState } from 'react';
+
+const useCoin = (label, initialState, options) => {
+  const [state, setState] = useState(initialState);
+  const Select = () => (
+    <>
+      <label>{label}</label>
+      <select>
+        <option value=''>-Select-</option>
+        {options.map((option) => (
+          <option key={option.code} value={option.code}>
+            {option.name}
+          </option>
+        ))}
+      </select>
+    </>
+  );
+  return [state, Select, setState];
+};
+
+export default useCoin;
