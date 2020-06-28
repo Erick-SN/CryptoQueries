21505afbe4f4da7ba43a25786198ac1d530e0aa6
Erick-SN <ericksn0707@gmail.com>
2020-06-28 10:17:05 -0500

    stylizing the Quote component

--- src/components/Quote.jsx
+++ src/components/Quote.jsx
@@ -1,25 +1,45 @@
 import React from 'react';
+import styled from '@emotion/styled';
+
+const Result = styled.div`
+  color: #fff;
+  font-family: Arial, Helvetica, sans-serif;
+`;
+
+const Info = styled.p`
+  font-size: 18px;
+  span {
+    font-weight: bold;
+  }
+`;
+const Price = styled.p`
+  font-size: 30px;
+  span {
+    font-weight: bold;
+  }
+`;
+
 const Quote = ({ result }) => {
   if (Object.keys(result).length === 0) return null;
   return (
     <>
-      <div>
-        <p>
+      <Result>
+        <Price>
           Price: <span>{result.PRICE}</span>
-        </p>
-        <p>
+        </Price>
+        <Info>
           Highest price of the day: <span>{result.HIGHDAY}</span>
-        </p>
-        <p>
+        </Info>
+        <Info>
           Lowest price of the day: <span>{result.LOWDAY}</span>
-        </p>
-        <p>
+        </Info>
+        <Info>
           Variation the last 24 hours: <span>{result.CHANGEPCT24HOUR}</span>
-        </p>
-        <p>
+        </Info>
+        <Info>
           Last update: <span>{result.LASTUPDATE}</span>
-        </p>
-      </div>
+        </Info>
+      </Result>
     </>
   );
 };
